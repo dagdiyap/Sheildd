cad19c8e0c617a3916a9429b00d4beb74cde4fa3
pranav <76247369+pranav-iitr@users.noreply.github.com>
2023-03-25 14:19:55 +0530

    intilized ml

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
+    'ML',
     
 ]
 
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- backend/urls.py
+++ backend/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
+from ML.views import index
 
 
 urlpatterns = [
+    path('',index),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 ]
